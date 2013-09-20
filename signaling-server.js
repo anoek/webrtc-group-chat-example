9f00a1627d7eade7700dc1ee1bef2e1e4ba67cb0
Akita Noek <akitanoek@gmail.com>
2013-09-20 18:51:46 -0400

    Code cleanup and documentation

--- signaling-server.js
+++ signaling-server.js
@@ -27,6 +27,16 @@ main.use(express.bodyParser());
 var channels = {};
 var sockets = {};
 
+/**
+ * Users will connect to the signaling server, after which they'll issue a "join"
+ * to join a particular channel. The signaling server keeps track of all sockets
+ * who are in a channel, and on join will send out 'addPeer' events to each pair
+ * of users in a channel. When clients receive the 'addPeer' even they'll begin
+ * setting up an RTCPeerConnection with one another. During this process they'll
+ * need to relay ICECandidate information to one another, as well as SessionDescription
+ * information. After all of that happens, they'll finally be able to complete
+ * the peer connection and will be streaming audio/video between eachother.
+ */
 io.sockets.on('connection', function (socket) {
     socket.channels = {};
     sockets[socket.id] = socket;
@@ -88,7 +98,6 @@ io.sockets.on('connection', function (socket) {
         console.log("["+ socket.id + "] relaying ICE candidate to [" + peer_id + "] ", ice_candidate);
 
         if (peer_id in sockets) {
-            /* In production code you'd want to probably limit what can be relayed using this mechanism. */
             sockets[peer_id].emit('iceCandidate', {'peer_id': socket.id, 'ice_candidate': ice_candidate});
         }
     });
@@ -99,7 +108,6 @@ io.sockets.on('connection', function (socket) {
         console.log("["+ socket.id + "] relaying session description to [" + peer_id + "] ", session_description);
 
         if (peer_id in sockets) {
-            /* In production code you'd want to probably limit what can be relayed using this mechanism. */
             sockets[peer_id].emit('sessionDescription', {'peer_id': socket.id, 'session_description': session_description});
         }
     });
